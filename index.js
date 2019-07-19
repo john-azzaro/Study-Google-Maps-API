28dda275447439a7755e2cf305dc749d0a45d1d0
John Azzaro <jazzaro@temple.edu>
2019-07-18 22:48:53 -0700

    added basic initialization function for google maps

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+function initMap() {
+  var city = {lat: -25.344, lng: 131.036};                                   // Location stored as a variable
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: city});               // The map, centered on the city of your choice at the given coordinates
+  var marker = new google.maps.Marker({position: city, map: map});            // position of marker
+}
